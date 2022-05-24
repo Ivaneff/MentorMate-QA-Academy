056c6423086abcc9c0531c0c85b9e9a45485feee
Ivo <ivaneff@yahoo.com>
2022-05-24 16:56:30 +0300

    New repository MentorMate

--- src/operatorsExpressions/NumbersDivisibleByFive.java
+++ src/operatorsExpressions/NumbersDivisibleByFive.java
@@ -2,6 +2,13 @@ package operatorsExpressions;
 
 public class NumbersDivisibleByFive {
     public static void main(String[] args) {
-
+        int[] arr = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+        for (int i : arr) {
+            if(i>150){
+                break;
+            } else if (i%5==0) {
+                System.out.print(i+" ");
+            }
+        }
     }
 }
