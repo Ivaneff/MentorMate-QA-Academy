33dbdc69db436fa30473d9fb6763315d29832d71
Ivo <ivaneff@yahoo.com>
2022-05-29 00:58:30 +0300

    New repository MentorMate

--- src/dataStructures/ReverseLinkedList.java
+++ src/dataStructures/ReverseLinkedList.java
@@ -1,4 +1,18 @@
 package dataStructures;
 
+import java.util.LinkedList;
+
 public class ReverseLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> lList = new LinkedList<>();
+        lList.add("Alpha");
+        lList.add("Beta");
+        lList.add("Gama");
+        lList.add("Delta");
+        LinkedList<String> rList = new LinkedList<>();
+        for (int i = lList.size()-1; i >=0 ; i--) {
+            rList.add(lList.get(i));
+        }
+        System.out.println(rList);
+    }
 }
