76d1945b6ad2d42a7e732325d543898488b2a5a4
Ivo <ivaneff@yahoo.com>
2022-06-26 23:41:23 +0300

    New repository MentorMate

---
+++ src/test/java/restAssuredHomework/LoggingAuthenticationTests.java
@@ -0,0 +1,36 @@
+package restAssuredHomework;
+
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.*;
+import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertEquals;
+
+public class LoggingAuthenticationTests {
+    @BeforeTest
+    public void setup() {
+        baseURI = "http://training.skillo-bg.com:3100";
+    }
+
+    @Test
+    public void loginTest() {
+
+        LoginDto2 loginDto = new LoginDto2();
+        loginDto.setUsernameOrEmail("Ivo1");
+        loginDto.setPassword("ivoivo1");
+
+        Response response = given().
+                log().all().
+                header("Content-Type", "application/json").
+                body(loginDto).
+                when().
+                post("/users/login").
+                then().
+                log().all().
+                extract().response();
+
+        int statusCode = response.statusCode();
+        assertEquals(statusCode, HttpStatus.SC_CREATED);
+
+    }
+}
